b8cccd9f3a58e548c108ede89204f6480ec57f27
lisa-desousa <lisa.desousamestre@gmail.com>
2026-05-25 13:10:01 +0200

    fetch data test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { useAllDogs } from "./hooks/useAllDogs";
+
 //STRUKTUR:
 
 //sökfällt
@@ -5,9 +7,14 @@
 //varje hund-kort är klickbart och leder till ett mer detaljerat kort
 
 function App() {
+  const { dogs } = useAllDogs();
+  console.log(dogs);
   return (
     <>
       <p className="underline text-7xl text-amber-950">tailwind test</p>
+      {dogs.map((item) => (
+        <h3 key={item.id}>{item.breed}</h3>
+      ))}
     </>
   );
 }
---
+++ frontend/src/types/Breed.ts
@@ -0,0 +1,11 @@
+export type Breed = {
+  id: number;
+  breed: string;
+  imageUrl: string | null;
+  lifeSpan: string | null;
+  description: string | null;
+  temperament: string | null;
+  breedOrigin: string | null;
+  createdAt: string;
+  updatedAt: string;
+};

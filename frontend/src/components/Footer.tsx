09d8285c55bfeabf865202cab5d740425e12cffe
Erik Eriksson Broomé <erik.o.eriksson@telia.com>
2026-06-02 18:35:54 +0200

    reworked the footer content wip

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,21 +2,65 @@
 export default function Footer() {
 
   return (
-  <footer className="w-full  border-t border-secondary h-30 sm:h-40 bg-white">
-    <div className="flex flex-row items-center justify-center mt-2 sm:mt-5">
-    <p className="text-sm">Github :</p>
-    <a
-    href="https://github.com/Liendea/DogApp"
-    target="_blank"
-    rel="noopener noreferrer"
-    >
-      DogApp
-    </a>
+  <footer className="w-full border-t border-secondary h-40 sm:h-50 bg-white">
+    <div className="flex flex-row justify-around mt-2 sm:mt-5">
+    <div className="flex flex-col">
+      <h3 className="font-bold">Links : </h3>
+        <a
+        href="https://github.com/Liendea/DogApp"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        <a
+        href="https://dog.ceo/dog-api/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          Image Source
+        </a>
     </div>
-      <p className="text-sm text-center">Application made by Erik, Linda, Lisa, José och Hannah.</p>
-      <p className="text-sm text-center">Dog images from : Dog.ceo</p>
-      <p className="text-sm text-center">Application deployed in RailWay</p>
-    <p className="text-sm font-bold text-center">@ DogApp 2026</p>
+    <div className="flex flex-col items-center">
+      <h3 className="font-bold">About us : </h3>
+      <a
+      href="https://github.com/Grayfox665"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+        Erik
+      </a>
+      <a
+      href="https://github.com/Liendea"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+        Linda
+      </a>
+      <a
+      href="https://github.com/JoseRaul-TR"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+        José
+      </a>
+      <a
+      href="https://github.com/hannasahlberg"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+        Hannah
+      </a>
+      <a
+      href="https://github.com/lisa-desousa"
+      target="_blank"
+      rel="noopner noreferrer"
+      >
+        Lisa
+      </a>
+    </div>
+    </div>
+    <p className="text-sm font-light text-center">@ DogApp 2026</p>
   </footer>
   )
 }

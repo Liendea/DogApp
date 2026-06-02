e13692e167dfadebbecf7440608decd15389301a
Erik Eriksson Broomé <erik.o.eriksson@telia.com>
2026-06-02 17:24:11 +0200

    moved footer to proper  folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import DogList from "./features/browseDogs/DogList";
 import Header from "./components/Header";
 import { SearchField } from "./features/search/searchField";
 import { useState } from "react";
-import Footer from "./features/footer/Footer";
+import Footer from "./components/Footer";
 
 function App() {
   // använd error och loading states från useGetDogs när det finns komponenter för dem

6e58282f5817bcbe219233343c31b62e0aa2509e
José_Raúl Tenza_Ramírez <jraultr@gmail.com>
2026-06-02 14:13:23 +0200

    Styled modal

--- frontend/src/features/dogDetails/CloseBtn.tsx
+++ frontend/src/features/dogDetails/CloseBtn.tsx
@@ -1,10 +1,15 @@
+// src/features/dogDetails/CloseBtn.tsx
+
 type CloseBtnProps = {
   onClick: () => void;
 };
 
 export default function CloseBtn({ onClick }: CloseBtnProps) {
   return (
-    <button onClick={onClick} className="border rounded p-2">
+    <button
+      onClick={onClick}
+      className="border border-primary text-primary font-sans text-xs w-fit rounded-xl px-8 py-2 hover:cursor-pointer hover:text-white hover:bg-primary transition-all"
+    >
       Close
     </button>
   );

5c41eb98f43b5bc71a0f05fb2eae8d8324125582
Linda <bengtsson-linda@outlook.com>
2026-06-02 15:42:08 +0200

    Skapade en generisk SvgIcon kompoenent som man kan använda som bas för att skapa svg ikoner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import { useGetDogs } from "./hooks/useGetDogs";
 import DogList from "./features/browseDogs/DogList";
 import { SearchField } from "./features/search/searchField";
+import { useState } from "react";
 
 function App() {
   // använd error och loading states från useGetDogs när det finns komponenter för dem
-  const { dogs } = useGetDogs();
+  const [query, setQuery] = useState<string>("");
+  const { dogs } = useGetDogs(query);
   return (
     <>
       <h1 className="text-xl font-bold text-center">Dog App</h1>
-      <SearchField onSearch={(query) => console.log(query)} />
+      <SearchField setQuery={setQuery} />
       <DogList dogs={dogs} />
     </>
   );

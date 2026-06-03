983ea2f0351bbb63dd78aadfc554e403a3b77eb8
José_Raúl Tenza_Ramírez <jraultr@gmail.com>
2026-06-03 08:24:05 +0200

    Corrected radius in modal's close btn

--- frontend/src/features/dogDetails/CloseBtn.tsx
+++ frontend/src/features/dogDetails/CloseBtn.tsx
@@ -8,7 +8,7 @@ export default function CloseBtn({ onClick }: CloseBtnProps) {
   return (
     <button
       onClick={onClick}
-      className="border border-primary text-primary font-sans text-xs w-fit rounded-xl px-8 py-2 hover:cursor-pointer hover:text-white hover:bg-primary transition-all"
+      className="border border-primary text-primary text-xs font-semibold w-fit rounded-3xl px-8 py-2 hover:cursor-pointer hover:text-white hover:bg-primary transition-all"
     >
       Close
     </button>

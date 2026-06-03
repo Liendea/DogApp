6da0ac17501891c86bbb19787a361ad6d14db02f
lisa-desousa <lisa.desousamestre@gmail.com>
2026-06-03 14:54:36 +1000

    error and loading components created and implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,26 @@ import DogList from "./features/browseDogs/DogList";
 import Header from "./components/Header";
 import { SearchField } from "./features/search/searchField";
 import { useState } from "react";
+import LoadingSpinner from "./reusable_components/loadingSpinner";
+import Error from "./reusable_components/error";
 
 function App() {
-  // använd error och loading states från useGetDogs när det finns komponenter för dem
   const [query, setQuery] = useState<string>("");
-  const { dogs } = useGetDogs(query);
+  const { dogs, loading, error } = useGetDogs(query);
+  const noDogs = !loading && !error && dogs.length === 0;
   return (
     <>
       <Header />
       <SearchField setQuery={setQuery} />
-      <DogList dogs={dogs} />
+      {error ? (
+        <Error message="Något gick fel, försök igen" />
+      ) : loading ? (
+        <LoadingSpinner />
+      ) : noDogs ? (
+        <Error message="Inga hundar matchade din sökning" />
+      ) : (
+        <DogList dogs={dogs} />
+      )}
     </>
   );
 }

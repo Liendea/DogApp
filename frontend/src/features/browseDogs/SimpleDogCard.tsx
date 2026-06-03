0d28c57ff04557ce87af93c6535f236878e4009c
lisa-desousa <lisa.desousamestre@gmail.com>
2026-06-03 11:21:50 +1000

    dog list styling added

--- frontend/src/features/browseDogs/SimpleDogCard.tsx
+++ frontend/src/features/browseDogs/SimpleDogCard.tsx
@@ -9,15 +9,17 @@ type CardProps = {
 export default function SimpleDogCard({ breed, image, onOpen }: CardProps) {
   const fallback = `https://placehold.net/default.png`;
   return (
-    <div className="border rounded p-4 flex flex-col gap-3">
-      <div className="w-full h-36">
+    <div className="border border-primary/20 rounded-[20px] p-4 flex flex-col gap-3 h-96 shadow-md shadow-primary-hover bg-white hover:shadow-lg hover:shadow-primary-hover hover:-translate-y-1 transition-all">
+      <div className="w-full h-66 rounded-[20px] overflow-hidden">
         <img
           src={image ?? fallback}
           alt={breed}
           className="w-full h-full object-cover"
         ></img>
       </div>
-      <h3 className="capitalize">{breed}</h3>
+      <h3 className="font-sans text-2xl font-bold capitalize text-black">
+        {breed}
+      </h3>
 
       {/* onclick -> öppna modal */}
       <ShowDetailsBtn onOpen={onOpen} />

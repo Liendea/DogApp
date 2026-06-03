8926f85e7287c82fae5a59e3623ba0af30e468db
Linda <bengtsson-linda@outlook.com>
2026-06-03 15:27:25 +0200

    Removed search text  onsmaller screens, changed placeholder text

--- frontend/src/features/search/components/SearchButton.tsx
+++ frontend/src/features/search/components/SearchButton.tsx
@@ -3,14 +3,15 @@ import SearchIcon from "../../../reusable_components/SearchIcon";
 type SearchButtonProps = {
   onClick?: () => void;
 };
+
 export default function SearchButton({ onClick }: SearchButtonProps) {
   return (
     <button
-      className="rounded-full bg-primary text-xsmall pr-4 text-white cursor-pointer transition-all flex items-center"
+      className="rounded-full bg-primary px-2 text-xsmall md:pr-4 text-white cursor-pointer transition-all flex justify-center items-center"
       onClick={onClick}
     >
       <SearchIcon width={40} height={40} />
-      Search
+      <span className="hidden md:inline">Search</span>
     </button>
   );
 }

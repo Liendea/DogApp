bebfd082cbda383cfe3929209abdec990b627d28
José_Raúl Tenza_Ramírez <jraultr@gmail.com>
2026-06-03 11:50:34 +0200

    Change btn 'Visa mer' to 'Details'

--- frontend/src/features/browseDogs/ShowDetailsBtn.tsx
+++ frontend/src/features/browseDogs/ShowDetailsBtn.tsx
@@ -8,7 +8,7 @@ export default function ShowDetailsBtn({ onOpen }: BtnProps) {
       className=" text-white font-semibold bg-primary text-xs w-fit self-end rounded-3xl px-8 py-2 hover:cursor-pointer hover:text-primary hover:bg-primary-hover transition-all"
       onClick={onOpen}
     >
-      Visa Mer
+      Details
     </button>
   );
 }
